fd8cbc73ac86e3b308bf8513ffe750a682c96ce0
AgustinChevallier <agustinjchevallier@gmail.com>
2025-04-16 18:48:20 -0300

    responsive + dark/light

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const boton = document.querySelector(".saludar");
 const mensaje = document.getElementById("mensaje");
+const switchModo = document.getElementById("modoSwitch");
+
+document.body.classList.add("dark-mode");
+
 
 boton.addEventListener("click", function () {
   const ahora = new Date();
   const hora = ahora.toLocaleTimeString(); 
 
-  mensaje.textContent = `¡Hola Agustín! Son las ${hora}. Bienvenido a tu página.`;
+  mensaje.textContent = `¡Hola invitado! Son las ${hora}. Bienvenido a mi página.`;
   console.log("Hola Consola! Bienvenido a tu página");
   alert(`¡Pop-up activado! Son las: ${hora}`);
 });
 
 mensaje.classList.add("glow");
+
+switchModo.addEventListener ("click", ()=> {
+  document.body.classList.toggle("dark-mode");
+  document.body.classList.toString("light-mode");
+
+  if (document.body.classList.contains ("dark-mode")){
+    switchModo.textContent = "cambiar a modo claro";
+  } else{
+    switchModo.textContent = "cambiar a modo oscuro";
+  }
+});
